a2164870276d2e7156513cc792fd1e913ec90dc0
williankargo <pinkuanxie@gmail.com>
2023-11-15 20:08:18 -0500

    Turn image_data to hash value

--- face_OTP_API/routes.py
+++ face_OTP_API/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 import logging
 import service
+import hashlib
 
 blueprint = Blueprint("routes", __name__)
 
@@ -8,6 +9,7 @@ blueprint = Blueprint("routes", __name__)
 @blueprint.route("/")
 def home():
     return "<h1>Hello World!</h1>"
+
 
 @blueprint.route("/verify", methods=["POST"])
 def verify():
@@ -24,7 +26,18 @@ def verify():
         if not img2_data.startswith("data:image"):
             img2_data = f"data:image/jpeg;base64,{img2_data}"
 
-        img1_path = "./OwnerData/pinkuan.jpg"  # owner's image path
+        img1_data = "./OwnerData/pinkuan.jpg"  # owner's image path
+        
+        '''
+        Turn image_data to hash value
+        '''
+        with open(img1_data, "rb") as img_file:
+            img1_data_base64 = img_file.read()
+        hash_obj = hashlib.sha256()
+        hash_obj.update(img1_data_base64)
+        hash_str = hash_obj.hexdigest()
+        img1_data_hash = hash_str  # TODO: you guys can use this as a key to sort the DB
+        logging.warn("img1_data_hash: %s ", img1_data_hash)
 
         model_name = input_args.get("model_name", "Facenet")
         detector_backend = input_args.get("detector_backend", "opencv")
@@ -33,7 +46,7 @@ def verify():
         align = input_args.get("align", True)
 
         verification = service.verify(
-            img1_path=img1_path,
+            img1_path=img1_data,
             img2_path=img2_data,
             model_name=model_name,
             detector_backend=detector_backend,
@@ -49,8 +62,6 @@ def verify():
     except Exception as e:
         logging.error(f"An error occurred: {e}")
         return (
-            jsonify(
-                {"error": str(e)}
-            ),
+            jsonify({"error": str(e)}),
             400,
         )
